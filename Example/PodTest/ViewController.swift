c6daad5a0f06c3a7ec1987f8b376c6180d61d042
na-khandaker <113911314+na-khandaker@users.noreply.github.com>
2022-11-27 15:45:16 +0600

    Pod init

--- Example/PodTest/ViewController.swift
+++ Example/PodTest/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import PodTest
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       let log = Logger()
+        log.testPrint()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ PodTest/Classes/Logger.swift
@@ -0,0 +1,18 @@
+//
+//  Logger.swift
+//  PodTest
+//
+//  Created by BCL-Device-11 on 27/11/22.
+//
+
+import Foundation
+
+public class Logger {
+    public init () {
+        
+    }
+    
+    public func testPrint() {
+        print("called from pod file",#function)
+    }
+}
